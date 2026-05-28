1f9efc640e2ebf60615be8e26692df7f4c4d0669
Jirrer <jrirrer@gmail.com>
2026-05-28 18:15:06 -0400

    built backend

--- backend/src/pullReport.py
+++ backend/src/pullReport.py
@@ -1,4 +1,4 @@
-import sqlite3, os
+import sqlite3, os, re
 
 from enum import Enum
 from dotenv import load_dotenv
@@ -7,28 +7,80 @@ from src.exceptions import *
 
 load_dotenv()
 
-class InputType(Enum):
-    MONTH = 1
-
 class ReturnType(Enum):
     JSON = 1
 
-def run(**data):
-    if not data: return None
+class Date:
+    def __init__(self, inputStr: str):
+        splitResult = re.split(r'[-/]', inputStr)
+        
+        if len(splitResult[0]) == 4 and len(splitResult[1]) == 2:
+            self.year, self.month = splitResult[0], splitResult[1]
 
-    if not data['userID']: raise KeyError
+        elif len(splitResult[0]) == 2 and len(splitResult[1]) == 4:
+            self.month, self.year = splitResult[0], splitResult[1]
 
-    if not data['inputType']: raise KeyError
+        else:
+            raise BadDateInput
+        
+    def __lt__(self, other):
+        if not isinstance(other, Date):
+            return NotImplemented
+        
+        if self.year != other.year:
+            return int(self.year) < int(other.year)
+        
+        else:
+            return int(self.month) < int(other.month)
+        
+    def ___gt__(self, other):
+        if not isinstance(other, Date):
+            return NotImplemented
+        
+        if self.year != other.year:
+            return int(self.year) > int(other.year)
+        
+        else:
+            return int(self.month) > int(other.month)
+        
+    def __eq__(self, value):
+        if not isinstance(value, Date):
+            raise NotImplemented
 
-    match data['inputType'].upper():
-        case InputType.MONTH.name: return getMonthReport(data)
-        case _ : raise KeyError
+        return int(self.month) == int(value.month) and int(self.year) == int(value.year)
 
-def getMonthReport(data: dict):
-    if not data['date']: raise KeyError
 
-    date = getDate(data['date'])
-    
+def run(userID: int, dateStartInput: str, dateEndInput: str, returnType: ReturnType) -> dict:
+    dateStart = Date(dateStartInput)
+    dateEnd = Date(dateEndInput)
+
+    try:
+        match returnType.upper():
+            case ReturnType.JSON.name: returnType = ReturnType.JSON
+            case _: returnType = ReturnType.JSON
+
+    except AttributeError:
+        returnType = ReturnType.JSON
+
+    output = {}
+
+    while True:
+        output[f'{dateStart.month}/{dateStart.year}'] = getDatabaseOutput(userID, dateStart)
+
+        match returnType:
+            case ReturnType.JSON: 
+                output[f'{dateStart.month}/{dateStart.year}'] = returnJson(output[f'{dateStart.month}/{dateStart.year}'])
+
+        if dateStart == dateEnd:
+            return output
+
+        if int(dateStart.month) < 12:
+            dateStart.month = str(int(dateStart.month) + 1)
+        else:
+            dateStart.month = '01'
+            dateStart.year = str(int(dateStart.year) + 1)  
+
+def getDatabaseOutput(userID, date: Date):
     categories = {'income': None, 'purchase': None, 'transfer': None}
 
     with sqlite3.connect(os.getenv('Database_Location')) as connection:
@@ -36,24 +88,13 @@ def getMonthReport(data: dict):
 
         for category in categories.keys():
             categories[category] = cursor.execute(
-                f"SELECT * FROM {category} WHERE user_id = ? AND strftime('%Y-%m', date) = ?;", (data['userID'], date)
-                ).fetchall()
+                f"SELECT * FROM {category} WHERE user_id = ? AND strftime('%Y-%m', date) = ?;", (userID, f'{date.year}-{date.month}')
+                ).fetchall()    
 
-    try:
-        match data['returnType'].upper():
-            case ReturnType.JSON.name: returnType = ReturnType.JSON
-            case _: returnType = ReturnType.JSON
-
-    except KeyError:
-        returnType = ReturnType.JSON
-
-    match returnType:
-        case ReturnType.JSON: return returnJson(categories)
-
-def getDate(possibleDate: str):
-    return possibleDate
+    return categories
 
 def returnJson(categories: dict) -> dict:
+    print(categories)
     purchases = [x[4] for x in categories['purchase']]
     incomes = [x[4] for x in categories['income']]
     external_transfers = [x[4] for x in categories['transfer'] if x[3].lower() == 'external']
@@ -83,3 +124,9 @@ def getCategories(arr: list) -> dict:
             output[category] = value
 
     return output
+
+if __name__ == "__main__":
+    test = run(1, Date('04/2026'), Date('04/2026'), None)
+
+    for x in test:
+        print(test[x])
