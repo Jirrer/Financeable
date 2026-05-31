3545996c1fdd179e00ab31926c58be831dd9e5a0
Jirrer <jrirrer@gmail.com>
2026-05-31 15:21:09 -0400

    updated gh action

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/financeable/frontend/',
+  base: '/Finiancable/frontend/',
   plugins: [react()],
   server: {
     proxy: {
