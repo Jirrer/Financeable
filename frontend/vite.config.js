b434cf34664c1e4adf4d67126168d375c253c406
John Irrer <jrirrer@gmail.com>
2026-05-31 15:39:41 -0400

    Update base path in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Financeable/frontend/',
+  base: '/Financeable/',
   plugins: [react()],
   server: {
     proxy: {
